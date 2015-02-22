555115798d980403cea76f7adace909c6a8a4577
Robert Haritonov <r@rhr.me>
2015-02-22 14:13:02 +0100

    Adding error handling

--- core/index.js
+++ core/index.js
@@ -10,31 +10,77 @@ var CrowdVoice = mongoose.model('CrowdVoice', {
     text: String
 });
 
+var resources = {
+    noConnection: 'MongoDB connection is inactive.',
+    dbError: 'Could not get information from DB.'
+};
+
+var config = {
+    statusCodes: {
+        OK: 200,
+        notFound: 404,
+        error: 500
+    }
+};
+
+var connectionErrorHandler = function(res){
+    res.status(config.statusCodes.notFound).send(resources.noConnection);
+};
+
+var DBErrorHandler = function(res, err){
+    res.status(config.statusCodes.error).send(resources.dbError);
+    console.log('Crowd-voice: ', resources.dbError, err);
+};
+
 var getAllCrowdVoiceData = function(req, res){
+    if (mongoose.connection.readyState === 0) {
+        connectionErrorHandler(res);
+        return;
+    }
 
     CrowdVoice.find(function(err, data){
-        if(!err) {
-            res.jsonp(data);
+        if (err) {
+            DBErrorHandler(res, err);
+            return;
         }
+
+        res.jsonp(data);
     });
 };
 
 var getCrowdVoice = function(req, res){
+    if (mongoose.connection.readyState === 0) {
+        connectionErrorHandler(res);
+        return;
+    }
+
     var specURI = req.query.pathToDataFile;
 
     CrowdVoice.find({specURI: specURI}, function(err, data){
-        if(!err) {
-            res.jsonp(data);
+        if (err) {
+            DBErrorHandler(res, err);
+            return;
         }
+
+        res.jsonp(data);
     });
 };
 
 var setCrowdVoice = function(req, res){
+    if (mongoose.connection.readyState === 0) {
+        connectionErrorHandler(res);
+        return;
+    }
 
     var specURI = req.query.pathToDataFile;
     var text = req.query.text;
 
     CrowdVoice.findOneAndUpdate({specURI: specURI}, {text: text}, function(err, data) {
+        if (err) {
+            DBErrorHandler(res, err);
+            return;
+        }
+
         if(data != null) {
 
             res.jsonp(data);
@@ -47,9 +93,12 @@ var setCrowdVoice = function(req, res){
             });
 
             crowdvoice.save(function (err, data) {
-                if (!err){
-                    res.jsonp(data);
+                if (err) {
+                    DBErrorHandler(res, err);
+                    return;
                 }
+
+                res.jsonp(data);
             });
         }
 
@@ -57,9 +106,19 @@ var setCrowdVoice = function(req, res){
 };
 
 var removeCrowdVoice = function(req, res){
+    if (mongoose.connection.readyState === 0) {
+        connectionErrorHandler(res);
+        return;
+    }
+
     var specURI = req.query.pathToDataFile;
 
     CrowdVoice.findOneAndRemove({specURI: specURI}, function(err, data) {
+        if (err) {
+            DBErrorHandler(res, err);
+            return;
+        }
+
         res.jsonp(data);
     });
 };
